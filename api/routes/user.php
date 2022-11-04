5c286b6f37451c44b14f4454fdb365c83796e847
John Paul Banera <104751512+johnpaul-sun@users.noreply.github.com>
2022-11-04 11:44:38 +0800

    [ELS2-016]-[Bugfix] Fix verify route redirect error (#11)

--- api/routes/user.php
+++ api/routes/user.php
@@ -11,6 +11,9 @@ Route::group(['prefix' => '/v1'], function () {
     Route::post('/forgot-password', [ForgotPasswordController::class, 'forgot'])->name('password.forgot');
     Route::get('/redirect', [ForgotPasswordController::class, 'redirect'])->name('password.reset');
     Route::post('/reset-password', [ForgotPasswordController::class, 'reset']);
+
+    Route::get('/email/verify', [VerificationController::class, 'verify'])->name('verification.verify');
+    Route::get('/email/resend', [VerificationController::class, 'resend'])->name('verification.resend');
   });
 });
 
@@ -18,8 +21,5 @@ Route::group(['middleware' => 'auth:sanctum','prefix' => '/v1'], function () {
   Route::group(['prefix' => '/user'], function () {
     Route::get('/', [UserController::class, 'index']);
     Route::get('/{user}', [UserController::class, 'show']);
-    
-    Route::get('/email/verify', [VerificationController::class, 'verify'])->name('verification.verify');
-    Route::get('/email/resend', [VerificationController::class, 'resend'])->name('verification.resend');
   });
 });
